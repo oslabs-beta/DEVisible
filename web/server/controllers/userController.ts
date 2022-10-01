9c661256d18f13e71bd6ca28c4395721ec209798
EdSheranaway <edenshirin@gmail.com>
2022-10-01 17:47:32 -0500

    inital server structure

---
+++ web/server/controllers/userController.ts
@@ -0,0 +1,6 @@
+// require model through prisma?
+export const userController = {};
+
+// userController.createUser = (req, res, next) => {};
+
+// module.exports = userController;
--- web/server/server.ts
+++ web/server/server.ts
@@ -1,1 +1,46 @@
-console.log('hello from server');
+import express, { Request, Response, NextFunction, Application } from 'express';
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
+
+const prisma = new PrismaClient();
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// TODO require routers
+const apiRouter = require('./routes/api');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, '../frontend')));
+
+// TODO route handlers
+app.use('/api', apiRouter);
+
+app.get('/', (req, res): void => {
+  res.status(200).sendFile(path.resolve(__dirname, '../frontend/index.html'));
+});
+
+// TODO get requests for reactrouter routes
+
+app.use('*', (req: Request, res: Response) => {
+  return res.status(404).send('The page you are looking for does not exist.');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr = {
+    log: 'GLOBAL ERROR HANDLER: caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  // console.log(errorObj.log); // -> this was the original
+  console.log(errorObj);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} (O_O)`);
+});
+
+module.exports = app;
