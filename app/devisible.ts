22c4d628968baaf1d4fc59f6a3c6ce0ededa057e
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-03 11:32:26 -0400

    wip

--- app/devisible.ts
+++ app/devisible.ts
@@ -1,1 +1,49 @@
-console.log('hello from app');
+import getFolderSize from 'get-folder-size';
+import { parse } from 'ts-command-line-args';
+import { gitToJs } from 'git-parse';
+
+const art = `
+██████████████████▓▓▒▒░░░░░░░░░░
+███████████████████████▓▒░░░░░░░
+██████████████████████████▓░░░░░
+████████████████████████████▒░░░
+████████████▓▒▒▒▒▒▒▓█████████▓░░
+██████████▒▒▒▒▒▒▒▓▓▓▒▒████████▒░
+█████████░▒░░▒▓▓▓▓▓▓▓▓▒▓██████▓░
+████████▒▓░░▓▓▓▓██▓▓▓▓▓░███████░
+████████▒▓▒▓▓▓████▓▓▓▓▓░███████░
+████████▓▒▓▓▓▓▓▓▓▓▓▓▓▓▒▓███████░
+████████▓░░▓▓▓▓▓▓▓▓▓▓▒▓███████▓░
+██████▒▒▓██▓▒▒▒▒▒▒▒▒▓█████████░░
+███▓▒▒███████████████████████▒░░
+▓▒▒▓████████████████████████▒░░░
+▒█████████████████████████▒░░░░░
+███████████████████████▓▒░░░░░░░
+`;
+
+interface Arguments {
+  apiKey: string;
+  buildPath: string;
+  url?: string;
+  help?: boolean;
+}
+
+const args = parse<Arguments>(
+  {
+    apiKey: String,
+    url: { type: String, optional: true },
+    buildPath: String,
+    help: {
+      type: Boolean,
+      optional: true,
+      alias: 'h',
+      description: 'Prints this usage guide',
+    },
+  },
+  {
+    helpArg: 'help',
+  }
+);
+console.log(art);
+
+console.log(args);
