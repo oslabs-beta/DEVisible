a193403237cebefd4bed72cfd14eee529f4a465c
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-03 17:43:45 -0400

    npm package functions written

--- app/devisible.ts
+++ app/devisible.ts
@@ -1,6 +1,10 @@
-import getFolderSize from 'get-folder-size';
+import fs from 'fs';
+import { promisify } from 'util';
+import fetch from 'node-fetch';
+import { spawn } from 'child_process';
 import { parse } from 'ts-command-line-args';
 import { gitToJs } from 'git-parse';
+import fastFolderSize from 'fast-folder-size';
 
 const art = `
 ██████████████████▓▓▒▒░░░░░░░░░░
@@ -28,10 +32,10 @@ interface Arguments {
   help?: boolean;
 }
 
-const args = parse<Arguments>(
+const { apiKey, url, buildPath } = parse<Arguments>(
   {
-    apiKey: String,
-    url: { type: String, optional: true },
+    apiKey: { type: String },
+    url: { type: String, optional: true, defaultValue: 'localhost' },
     buildPath: String,
     help: {
       type: Boolean,
@@ -44,6 +48,47 @@ const args = parse<Arguments>(
     helpArg: 'help',
   }
 );
-console.log(art);
+function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return '0 Bytes';
 
-console.log(args);
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
+const sendData = async (buildTime: number) => {
+  // getFolderSize(buildPath, (err, size) => console.log(size));
+  await calculateBuildSize();
+
+  async function calculateBuildSize() {
+    const getFolderSize = promisify(fastFolderSize);
+    try {
+      const size = await getFolderSize(buildPath);
+      console.log(formatBytes(size!));
+    } catch (err) {
+      console.error('Error reading build directory: ' + err);
+    }
+  }
+};
+
+const start = Date.now();
+const buildProc = spawn('node', ['dist/dummy.js']);
+
+buildProc.stdout.on('data', (data) => {
+  console.log(data);
+});
+
+buildProc.stderr.on('data', (data) => {
+  console.log({ data });
+});
+
+buildProc.on('close', (code) => {
+  console.log(`build completed with code ${code}`);
+  const buildTime = Date.now() - start;
+  console.log(`Build time: ${buildTime}ms`);
+  sendData(buildTime);
+});
