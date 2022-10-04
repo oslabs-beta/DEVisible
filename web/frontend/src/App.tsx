46609486373065f3208dbee1f6dcf615d6aaf597
JP Moregain <j.p.moregain15@gmail.com>
2022-10-04 19:32:00 -0400

    Add app component level auth state, navbar conditional rendering, navbar dropdown, routing from dropdown menu. Create and style login page

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,27 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-// import "./stylesheets/App.css";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
-import Dashboard from "./components/Dashboard";
-import { Box } from "@mui/system";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { Box } from '@mui/system';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
 import Register from './components/Register';
 import Account from './components/Account';
 import Recovery from './components/Recovery';
-import { useState } from "react";
-import React from "react";
 
 function App(): JSX.Element {
   // state to track whether user has been authenticated or not -> will be prop drilled to child components
   const [auth, setAuth] = useState<boolean>(false);
 
   return (
-    <Box height={"100vh"} bgcolor={"primary.light"}>
+    <Box height={'100vh'} bgcolor={'primary.light'}>
       <BrowserRouter>
-      <Navbar auth={auth} setAuth={setAuth}/>
+        <Navbar auth={auth} setAuth={setAuth} />
         <Routes>
           <Route path="/" element={<Register />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Dashboard />} />
           <Route path="/account" element={<Account />} />
           <Route path="/recovery" element={<Recovery />} />
--- web/frontend/src/components/Dashboard.tsx
+++ web/frontend/src/components/Dashboard.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { styled } from "@mui/material/styles";
-import { Grid, Paper } from "@mui/material";
-import RepoItem from "./RepoItem";
-import Loader from "./Loader";
+import React, { useState, useEffect } from 'react';
+import { Grid } from '@mui/material';
+import RepoItem from './RepoItem';
+import Loader from './Loader';
 
 // const Item = styled(Paper)(({ theme }) => ({
 //   backgroundColor: theme.palette.primary.main,
@@ -11,27 +10,28 @@ import Loader from "./Loader";
 //   textAlign: 'center',
 //   color: theme.palette.primary.contrastText,
 // }));
-const Dashboard = () => {
+function Dashboard() {
   const [mockLoading, setMockLoading] = useState(true); //used for loader testing
   const numCards = 8;
   const cards = [];
   for (let i = 0; i < numCards; i++) {
-    cards.push(<RepoItem key={i}/>);
+    cards.push(<RepoItem key={i} />);
   }
   const handleStopMockLoading = () => {
     setMockLoading(false);
-  }
+  };
   //used for loader testing
   useEffect(() => {
     setTimeout(handleStopMockLoading, 2000);
-  }, [])
+  }, []);
   return (
     <div className="dashboard-container">
-      {mockLoading ? <Loader color={'orange'}/> : (
-      <Grid display="flex" justifyContent="center" container spacing={2}>
-        {cards}
-      </Grid>
-
+      {mockLoading ? (
+        <Loader color={'orange'} />
+      ) : (
+        <Grid display="flex" justifyContent="center" container spacing={2}>
+          {cards}
+        </Grid>
       )}
     </div>
   );
