8e06b7b3a120228a593ae7da954419186887af8c
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-05 12:09:22 -0400

    parse last git hash

--- app/devisible.ts
+++ app/devisible.ts
@@ -3,8 +3,10 @@ import { promisify } from 'util';
 import fetch from 'node-fetch';
 import { spawn } from 'child_process';
 import { parse } from 'ts-command-line-args';
+import yaml from 'js-yaml';
 import { gitToJs } from 'git-parse';
 import fastFolderSize from 'fast-folder-size';
+import path from 'path';
 
 const art = `
 ██████████████████▓▓▒▒░░░░░░░░░░
@@ -29,11 +31,13 @@ interface Arguments {
   apiKey: string;
   buildPath: string;
   command: string;
+  gitRoot?: string;
+  lockfile?: string | undefined;
   url?: string;
   help?: boolean;
 }
 
-const { apiKey, url, buildPath, command } = parse<Arguments>(
+const { apiKey, url, buildPath, command, gitRoot, lockfile } = parse<Arguments>(
   {
     apiKey: { type: String, alias: 'k' },
     url: {
@@ -44,6 +48,13 @@ const { apiKey, url, buildPath, command } = parse<Arguments>(
     },
     buildPath: { type: String, alias: 'p' },
     command: { type: String, alias: 'c' },
+    gitRoot: {
+      type: String,
+      alias: 'g',
+      optional: true,
+      defaultValue: process.cwd(),
+    },
+    lockfile: { type: String, alias: 'l', optional: true, defaultValue: '' },
     help: {
       type: Boolean,
       optional: true,
@@ -70,16 +81,80 @@ function formatBytes(bytes: number, decimals = 2) {
 const sendData = async (buildTime: number) => {
   // getFolderSize(buildPath, (err, size) => console.log(size));
   const buildSize = await calculateBuildSize();
+  await parseLockfile();
+  // const gitInfo = await parseGitRepo();
+};
 
-  async function calculateBuildSize() {
-    const getFolderSize = promisify(fastFolderSize);
-    try {
-      const size = await getFolderSize(buildPath);
-      console.log('Build size: ' + formatBytes(size!));
-      return size;
-    } catch (err) {
-      console.error('Error reading build directory: ' + err);
-    }
+async function calculateBuildSize() {
+  const getFolderSize = promisify(fastFolderSize);
+  try {
+    const size = await getFolderSize(buildPath);
+    console.log('Build size: ' + formatBytes(size!));
+    return size;
+  } catch (err) {
+    console.error('Error reading build directory: ' + err);
+  }
+}
+
+async function parsePackageJson() {}
+
+// currently only parsing the true package.json
+// if we want to try to parse the various types of lockfile, the skeleton is here
+// ↓↓↓
+
+// async function parseLockfile() {
+//   let file: string;
+//   let type: 'JSON' | 'YAML' | undefined;
+//   if (lockfile) {
+//     file = await fs.promises.readFile(lockfile, { encoding: 'utf-8' });
+//   }
+//   // if the user hasn't manually specified their lockfile to parse dependencies, we'll look for it ourselves
+//   if (!lockfile) {
+//     const packageLockPromise = fs.promises.readFile('./package-lock.json', {
+//       encoding: 'utf-8',
+//     });
+//     const yarnLockPromise = fs.promises.readFile('./yarn.lock', {
+//       encoding: 'utf-8',
+//     });
+
+//     const [packageLock, yarnLock] = await Promise.allSettled([
+//       packageLockPromise,
+//       yarnLockPromise,
+//     ]);
+//     if (packageLock.status === 'fulfilled' && yarnLock.status === 'fulfilled')
+//       throw new Error(
+//         'Multiple lockfiles found! Please specify the lockfile to use as a command line argument'
+//       );
+//     // file = packageLock.status === 'fulfilled' ? packageLock.value : yarnLock.value
+//     if (packageLock.status === 'fulfilled') {
+//       file = packageLock.value;
+//       type = 'JSON';
+//     } else if (yarnLock.status === 'fulfilled') {
+//       file = yarnLock.value;
+//       type = 'YAML';
+//     } else
+//       throw new Error(
+//         `Error reading lockfile. DEVisible supports package-lock.json and yarn.lock.
+//          Please call this program from the folder that the lockfile resides in or pass an absolute path to the lockfile as an argument.`
+//       );
+//     console.log(file);
+//     if (type === 'JSON') {
+//       return JSON.parse(file).packages.map((pkg) => {
+//         if (pkg) return { [pkg]: pkg.version };
+//       });
+//     }
+//   }
+// }
+
+const parseGitRepo = async () => {
+  try {
+    const commits = await gitToJs(gitRoot!);
+    const { hash } = commits[0];
+    return { hash };
+  } catch {
+    throw new Error(
+      'Error reading Git history. If this package is not being called from the git root, please specify it as an argument'
+    );
   }
 };
 
