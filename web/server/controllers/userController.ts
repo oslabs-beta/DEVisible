16d0cf4bcb2a9e82f0f34bdeb9dd6ec5c378d8e1
EdSheranaway <edenshirin@gmail.com>
2022-10-04 20:54:08 -0500

    API token generation with encryption and decryption

--- web/server/controllers/userController.ts
+++ web/server/controllers/userController.ts
@@ -3,9 +3,12 @@ import { Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import crypto from 'crypto';
 
+// environmental variables
 dotenv.config();
 const { JWT_SECRET } = process.env;
+const { ALGORITHM } = process.env;
 
 const prisma = new PrismaClient();
 interface UserController {
@@ -13,14 +16,43 @@ interface UserController {
   verifyUser: (req: Request, res: Response, next: NextFunction) => void;
   assignJwt: (req: Request, res: Response, next: NextFunction) => void;
   verifyJwt: (req: Request, res: Response, next: NextFunction) => void;
+  getToken: (req: Request, res: Response, next: NextFunction) => void;
 }
 
 // TODO think about how we give our api token
+const initVector = crypto.randomBytes(16);
+const securityKey = crypto.randomBytes(32);
+
+const encryptAPIToken = (): string => {
+  const unencryptedToken = crypto.randomUUID();
+
+  const cipher = crypto.createCipheriv(
+    ALGORITHM as string,
+    securityKey,
+    initVector
+  );
+
+  let encryptedToken = cipher.update(unencryptedToken, 'utf-8', 'hex');
+  encryptedToken += cipher.final('hex');
+  return encryptedToken;
+};
+
+const decryptAPIToken = (encryptedToken: string): string => {
+  const decipher = crypto.createDecipheriv(
+    ALGORITHM as string,
+    securityKey,
+    initVector
+  );
+  decipher.setAutoPadding(false);
+  let decryptedToken = decipher.update(encryptedToken, 'hex', 'utf-8');
+  decryptedToken += decipher.final('utf-8');
+  return decryptedToken;
+};
 
 const userController: UserController = {
   createUser: async (req, res, next) => {
     try {
-      const { username, plainPassword, email, APIToken } = req.body;
+      const { username, plainPassword, email } = req.body;
 
       if (!username || !plainPassword || !email) {
         return next({
@@ -29,6 +61,8 @@ const userController: UserController = {
         });
       }
       const passwordHash = await bcrypt.hash(plainPassword, 10);
+
+      const APIToken = encryptAPIToken();
 
       const newUser = await prisma.user.create({
         data: {
@@ -60,27 +94,26 @@ const userController: UserController = {
         });
       }
 
-      const loggedInUser = await prisma.user.findFirst({
+      const loggedInUser = await prisma.user.findFirstOrThrow({
         where: {
           username,
         },
       });
 
-      if (loggedInUser) {
-        const validPassword = await bcrypt.compare(
-          plainPassword,
-          loggedInUser.passwordHash
-        );
-        if (validPassword) {
-          res.locals.user = loggedInUser?.username;
-        } else {
-          return next({
-            log: 'null',
-            status: 401,
-            message: 'Invalid username and or password',
-          });
-        }
+      const validPassword = await bcrypt.compare(
+        plainPassword,
+        loggedInUser.passwordHash
+      );
+      if (validPassword) {
+        res.locals.user = loggedInUser?.username;
+      } else {
+        return next({
+          log: 'null',
+          status: 401,
+          message: 'Invalid username and or password',
+        });
       }
+
       return next();
     } catch (error) {
       return next({
@@ -115,6 +148,28 @@ const userController: UserController = {
     return verified
       ? next()
       : next({ status: 403, message: 'Unauthorized request' });
+  },
+  getToken: async (req, res, next) => {
+    try {
+      const { data } = res.locals.jwt;
+
+      const loggedInUser = await prisma.user.findFirstOrThrow({
+        where: {
+          username: data,
+        },
+      });
+      const { APIToken } = loggedInUser;
+      const decryptedToken = decryptAPIToken(APIToken);
+      res.locals.API = decryptedToken;
+
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in userController.getToken ${error}`,
+        status: 401,
+        message: `Error has occured in userController.getToken. ERROR: You're not logged in ${error}`,
+      });
+    }
   },
 };
 
