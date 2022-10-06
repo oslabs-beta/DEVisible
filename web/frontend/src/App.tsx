bcfcb3beb393ccacf8a47de356a851eced494392
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-06 13:38:50 -0400

    GET /login on first mount to set state

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/system';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
@@ -12,6 +12,19 @@ import { User } from './types';
 function App(): JSX.Element {
   // state to track whether user has been authenticated or not -> will be prop drilled to child components
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    fetch('/userAPI/login')
+      .then((res) => res.json())
+      .then((responseObj) => {
+        if (responseObj.data) {
+          setUser(responseObj.data);
+        } else {
+          setUser(null);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <Box height="100vh" bgcolor="primary.light">
