e21f1e70026c2290ad5ba23a16fc3a451deba929
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-09 19:25:13 -0400

    last 3 commits basic testing framework

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+APIKEY = `cat api_key.txt`
+
 while read repo; do
   git clone $repo test_build 
   cd test_build
   npm i
-  node ../../app/dist/devisible.js 
+  git checkout HEAD~2
+  node ../../app/dist/devisible.js -k "$APIKEY" -b "client/dist" -c "npm run build"
+  git checkout HEAD~1
+  node ../../app/dist/devisible.js -k "$APIKEY" -b "client/dist" -c "npm run build"
+  git checkout HEAD
+  node ../../app/dist/devisible.js -k "$APIKEY" -b "client/dist" -c "npm run build"
   cd ..
-  rm -rf repo
+  rm -rf test_build
 done < test_repos.txt
