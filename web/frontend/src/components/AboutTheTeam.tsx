72b255daca984db83fb63c54c8717edfe24fba95
JP Moregain <j.p.moregain15@gmail.com>
2022-10-13 13:45:27 -0400

    Complete styling about the team page

--- web/frontend/src/components/AboutTheTeam.tsx
+++ web/frontend/src/components/AboutTheTeam.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { styled } from '@mui/system';
 import CardMedia from '@mui/material/Card';
+import { Typography, Divider } from '@mui/material';
 import theme from '../theme';
 import '../stylesheets/abouttheteam.css';
 import Michael from '../assets/Michael.png';
 import Justin from '../assets/Justin.png';
 import Eden from '../assets/Eden.png';
 import Tanner from '../assets/Tanner.png';
+import JustinQR from '../assets/JustinQR.svg';
+import MichaelQR from '../assets/michaelQR.svg';
+import EdenQR from '../assets/EdenQR.svg';
+import TannerQR from '../assets/TannerQR.svg';
 
 function AboutTheTeam() {
   const CardStyle = styled(CardMedia)({
@@ -14,9 +19,9 @@ function AboutTheTeam() {
     flexDirection: 'column',
     alignItems: 'center',
     padding: '5% 0',
-    marginRight: '5%',
     height: 'max-content',
     backgroundColor: theme.palette.secondary.main,
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0.19)',
   });
   const NameStyled = styled('p')({
     fontSize: '1.5em',
@@ -35,55 +40,68 @@ function AboutTheTeam() {
   });
 
   return (
-    <div className="aboutTheTeam">
+    <div
+      className="container"
+      style={{
+        backgroundColor: `${theme.palette.primary.light}`,
+      }}
+    >
       <div
-        className="installation"
+        className="teamContainer"
         style={{
-          backgroundColor: `${theme.palette.primary.dark}`,
+          backgroundColor: theme.palette.primary.dark,
         }}
       >
-        <div className="installContainer">
-          <div className="title">Meet The Team</div>
-          <div className="instructions">
-            <ul
-              id="team"
-              style={{
-                color: `${theme.palette.secondary.dark}`,
-              }}
-            >
-              <li className="instruction">
-                <CardStyle>
-                  <img src={Michael} alt="Michael FanArt" />
-                  <NameStyled>Michael Sarkisian</NameStyled>
-                  <TitleStyled>Code Wizard</TitleStyled>
-                  <QuoteStyled>日本語が読みますか 上手ですね！</QuoteStyled>
-                </CardStyle>
-              </li>
-              <li className="instruction">
-                <CardStyle>
-                  <img src={Justin} alt="Justin FanArt" />
-                  <NameStyled>Justin Mendonca</NameStyled>
-                  <TitleStyled>Michael Sarkisian Impersonator</TitleStyled>
-                  <QuoteStyled>Touched grass this week</QuoteStyled>
-                </CardStyle>
-              </li>
-              <li className="instruction">
-                <CardStyle>
-                  <img src={Eden} alt="Eden FanArt" />
-                  <NameStyled>Eden Shirin</NameStyled>
-                  <TitleStyled>Executive Hebrew Speaker</TitleStyled>
-                  <QuoteStyled>Speaks Hebrew</QuoteStyled>
-                </CardStyle>
-              </li>
-              <li className="instruction">
-                <CardStyle>
-                  <img src={Tanner} alt="Tanner FanArt" />
-                  <NameStyled>Tanner Hesterman</NameStyled>
-                  <TitleStyled>Average Human</TitleStyled>
-                  <QuoteStyled>Scrum Lord</QuoteStyled>
-                </CardStyle>
-              </li>
-            </ul>
+        <div
+          id="headerContainer"
+          style={{ borderBottom: `1px solid ${theme.palette.secondary.light}` }}
+        >
+          <Typography id="header" color="secondary.main">
+            Meet The Team
+          </Typography>
+        </div>
+        <div className="teamMember">
+          <CardStyle>
+            <img src={Michael} alt="Michael FanArt" />
+            <NameStyled>Michael Sarkisian</NameStyled>
+            <TitleStyled>Code Wizard</TitleStyled>
+            <QuoteStyled>日本語が読みますか 上手ですね！</QuoteStyled>
+          </CardStyle>
+          <div className="qrContainer">
+            <img src={MichaelQR} alt="Michael's LinkedIn" />
+          </div>
+        </div>
+        <div className="teamMember">
+          <CardStyle>
+            <img src={Justin} alt="Justin FanArt" />
+            <NameStyled>Justin Mendonca</NameStyled>
+            <TitleStyled>Michael Sarkisian Impersonator</TitleStyled>
+            <QuoteStyled>Touched grass this week</QuoteStyled>
+          </CardStyle>
+          <div className="qrContainer">
+            <img src={JustinQR} alt="Michael's LinkedIn" />
+          </div>
+        </div>
+        <div className="teamMember">
+          <CardStyle>
+            <img src={Eden} alt="Eden FanArt" />
+            <NameStyled>Eden Shirin</NameStyled>
+            <TitleStyled>Executive Hebrew Speaker</TitleStyled>
+            <QuoteStyled>Speaks Hebrew</QuoteStyled>
+          </CardStyle>
+          <div className="qrContainer">
+            <img src={EdenQR} alt="Michael's LinkedIn" />
+          </div>
+        </div>
+        <div className="teamMember">
+          <CardStyle>
+            <img src={Tanner} alt="Tanner FanArt" />
+            <NameStyled>Tanner Hesterman</NameStyled>
+            <TitleStyled>Average Human</TitleStyled>
+            <QuoteStyled>Scrum Lord</QuoteStyled>
+          </CardStyle>
+          <div className="qrContainer">
+            <img src={TannerQR} alt="Michael's LinkedIn" />
           </div>
         </div>
       </div>
