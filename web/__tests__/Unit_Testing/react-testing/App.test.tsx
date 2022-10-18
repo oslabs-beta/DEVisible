c037fe970181d8c46ec44a6f9cb2971d32c2306d
EdSheranaway <edenshirin@gmail.com>
2022-10-17 20:00:59 -0500

    app unit testing

---
+++ web/__tests__/Unit_Testing/react-testing/App.test.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, RenderResult } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
+import renderer from 'react-test-renderer';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+
+import App from '../../../frontend/src/App';
+
+const renderWithRouter = (ui: JSX.Element, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return {
+    user: userEvent.setup(),
+    ...render(ui),
+  };
+};
+let documentBody: RenderResult;
+
+describe('Unit testing App component', () => {
+  describe('React-Router route testing', () => {
+    it('Renders landing page on start', () => {
+      // Page content for default route
+      const { user } = renderWithRouter(<App />);
+      expect(
+        screen.getByText(
+          /Make tracking your microservice architecture a micro-hassle/i
+        )
+      ).toBeInTheDocument();
+    });
+
+    it('Renders Login component when on /login', () => {
+      const route = '/login';
+      const { user } = renderWithRouter(<App />, { route });
+      expect(screen.getByLabelText(/Email address/i)).toBeInTheDocument();
+      expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
+    });
+
+    it('Renders Register component when on /signup', () => {
+      const route = '/signup';
+      const { user } = renderWithRouter(<App />, { route });
+      expect(screen.getByLabelText(/Username/i)).toBeInTheDocument();
+      expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
+    });
+
+    it('Renders About component when on /about', () => {
+      const route = '/about';
+      const { user } = renderWithRouter(<App />, { route });
+      expect(screen.getByText(/^Michael Sarkisian$/)).toBeInTheDocument();
+      expect(screen.getByText(/^Justin Mendonca$/)).toBeInTheDocument();
+      expect(screen.getByText(/^Eden Shirin$/)).toBeInTheDocument();
+      expect(screen.getByText(/^Tanner Hesterman$/)).toBeInTheDocument();
+    });
+  });
+});
--- web/setupTests.js
+++ web/setupTests.js
@@ -1,5 +1,3 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const fetch = (...args) =>
-//   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 require('jest-fetch-mock').enableMocks();
