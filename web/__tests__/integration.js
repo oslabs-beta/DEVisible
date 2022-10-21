41da3520313bb07a49371cc15b171903e9aa0db9
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-21 15:08:09 -0400

    api token and GET /login tests

--- web/__tests__/integration.js
+++ web/__tests__/integration.js
@@ -101,4 +101,45 @@ describe('User functionality', () => {
         );
     });
   });
+
+  describe('GET cookie user functionality', () => {
+    const agent = supertest.agent(server);
+    const body = {
+      email: 'test@test.com',
+      plainPassword: 'test1',
+    };
+    beforeAll(async () => {
+      await agent.post('/userAPI/login').send(body);
+    });
+    it("reads back the user's username and id when invoked with a valid cookie", () => {
+      return agent
+        .get('/userAPI/login')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        .expect((res) => {
+          expect(res.body.id).toBeDefined();
+          expect(res.body.username).toBe('test');
+        });
+    });
+    it('responds with a 401 status if the cookie is invalid', () => {
+      return supertest(server).get('/userAPI/login').expect(401);
+    });
+  });
+
+  describe('API Token functionality', () => {
+    const agent = supertest.agent(server);
+    const body = {
+      email: 'test@test.com',
+      plainPassword: 'test1',
+    };
+    beforeAll(async () => {
+      await agent.post('/userAPI/login').send(body);
+    });
+    it("responds with the user's API token if requested with a valid cookie", () => {
+      return agent.get('/userAPI/getToken').expect(200);
+    });
+    it('responds with a 403 status when not logged in', () => {
+      return supertest(server).get('/userAPI/getToken').expect(403);
+    });
+  });
 });
