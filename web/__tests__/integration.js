cf9be459a9e74fc90d3369569fde23eeefe507b8
Michael Sarkisian <msarkisian1996@gmail.com>
2022-10-22 08:19:45 -0400

    webapi tests

--- web/__tests__/integration.js
+++ web/__tests__/integration.js
@@ -12,7 +12,7 @@ const pool = new pg.Pool({
 });
 
 // eslint-disable-next-line no-undef
-jest.setTimeout(15000);
+jest.setTimeout(30000);
 
 xdescribe('User functionality', () => {
   beforeAll(async () => {
@@ -152,7 +152,7 @@ xdescribe('User functionality', () => {
   });
 });
 
-describe('App functionality', () => {
+xdescribe('App functionality', () => {
   let apiToken;
   const agent = supertest.agent(server);
   const body = {
@@ -336,5 +336,111 @@ describe('App functionality', () => {
       commitHash: 'A1B2C3D5',
     };
     return supertest(server).post('/app').send(appBody).expect(400);
+  });
+});
+
+describe('Web API functionality', () => {
+  const agent = supertest.agent(server);
+  const agent2 = supertest.agent(server);
+  const body = {
+    username: 'test',
+    plainPassword: 'test1',
+    email: 'test@test.com',
+  };
+  const body2 = {
+    username: 'test2',
+    plainPassword: 'test2',
+    email: 'test2@test.com',
+  };
+  beforeAll(async () => {
+    await pool.query('DELETE FROM "Build";');
+    await pool.query('DELETE FROM "Repo";');
+    await pool.query('DELETE FROM "User";');
+    await agent.post('/userAPI/signup').send(body);
+    const res = await agent.get('/userAPI/getToken');
+    const apiToken = res.body;
+    const appBody = {
+      apiKey: apiToken,
+      buildTime: 2000,
+      buildSize: 3000,
+      repoName: 'test-repo',
+      dependencies: [
+        { name: 'testdep', version: '0.0.1', isDevDependency: true },
+        { name: 'testdep2', version: '0.0.2', isDevDependency: false },
+      ],
+      commitHash: 'A1B2C3D4',
+    };
+
+    await supertest(server).post('/app').send(appBody);
+    await supertest(server)
+      .post('/app')
+      .send({ ...appBody, commitHash: 'A1B2C3D5', buildSize: 4000 });
+    await supertest(server)
+      .post('/app')
+      .send({ ...appBody, repoName: 'test-repo2' });
+    await agent2.post('/userAPI/signup').send(body2);
+    const res2 = await agent2.get('/userAPI/getToken');
+    await supertest(server)
+      .post('/app')
+      .send({ ...appBody, apiKey: res2.body });
+  });
+  it('responds with a 200 status and a list of user repos when GET to /webAPI/user is invoked with a valid cookie', () => {
+    agent
+      .get('/webAPI/user')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.includes((repo) => repo.name === 'test-repo')).toBe(
+          true
+        );
+      });
+  });
+
+  it('responds with a 401 status when GET to /webAPI/user is invoked with an invalid cookie', () => {
+    return supertest(server).get('/webAPI/user').expect(401);
+  });
+
+  it('allows the user to update their dependency preferences by POSTing /webAPI/userDeps', () => {
+    const deps = [
+      { name: 'testdep', version: '0.0.1' },
+      { name: 'testdep2', version: '0.0.2' },
+    ];
+    return agent
+      .post('/webAPI/userDeps')
+      .send({ depPrefs: deps })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toStrictEqual(deps);
+      });
+  });
+
+  it('allows the user to retrieve their dependency preferences by GETting /webAPI/userDeps', () => {
+    const deps = [
+      { name: 'testdep', version: '0.0.1' },
+      { name: 'testdep2', version: '0.0.2' },
+    ];
+    return agent
+      .get('/webAPI/userDeps')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toStrictEqual(deps);
+      });
+  });
+
+  it('responds with a 401 status when GET to /webAPI/userDeps is invoked with an invalid cookie', () => {
+    return supertest(server).get('/webAPI/userDeps').expect(401);
+  });
+
+  it('responds with a 401 status when POST to /webAPI/userDeps is invoked with an invalid cookie', () => {
+    return supertest(server).post('/webAPI/userDeps').expect(401);
+  });
+
+  it('allows the user to delete a repo by ID by DELETEing /webAPI/repo/:repoID', async () => {
+    const response = await agent.get('/webAPI/user');
+    return agent.delete(`/webAPI/repo/${response.body[0].id}`).expect(204);
+  });
+
+  it('prevents the user from deleting a repo by ID by DELETEing /webAPI/repo/:repoID if the repo does not belong to the user', async () => {
+    const response = await agent2.get('/webAPI/user');
+    return agent.delete(`/webAPI/repo/${response.body[0].id}`).expect(403);
   });
 });
