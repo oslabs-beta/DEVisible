8ee3de5b94e09fba07d05143c21dd81a363aa45b
JP Moregain <j.p.moregain15@gmail.com>
2022-10-24 12:40:15 -0400

    Login page error formatting and responsive styling

--- web/frontend/src/components/Login.tsx
+++ web/frontend/src/components/Login.tsx
@@ -24,7 +24,7 @@ function Login({ user, setUser }: Props) {
     <Box
       bgcolor="primary.main"
       style={{ border: `3px solid ${theme.palette.secondary.main}` }}
-      className="error"
+      id="error"
     >
       {error}
     </Box>
@@ -52,112 +52,110 @@ function Login({ user, setUser }: Props) {
   }
 
   return (
-    <>
-      {errorNotification}
-      <div className="container">
-        <Box bgcolor="primary.main" className="loginFormBox">
-          <div className="logoContainer">
-            <img className="logo" src={OrangeD} alt="DEVisible Icon Orange" />
-          </div>
-          <div className="formContainer">
-            <form
-              className="loginForm"
-              onSubmit={(e) => {
-                logMeIn(e);
+    <div className="container">
+      <Box bgcolor="primary.main" className="loginFormBox">
+        <div id="loginLogoContainer">
+          <img id="loginLogo" src={OrangeD} alt="DEVisible Icon Orange" />
+        </div>
+        <div className="formContainer">
+          {errorNotification}
+          <form
+            className="loginForm"
+            onSubmit={(e) => {
+              logMeIn(e);
+            }}
+          >
+            <TextField
+              sx={{
+                input: { color: 'white' },
+                '& .MuiInputLabel-root': {
+                  color: theme.palette.secondary.main,
+                }, //  styles the label
+                '& .MuiOutlinedInput-root': {
+                  '& > fieldset': {
+                    borderColor: theme.palette.secondary.main,
+                  },
+                },
+                '& .MuiOutlinedInput-root:hover': {
+                  '& > fieldset': {
+                    borderColor: theme.palette.secondary.main,
+                  },
+                },
+                '& .MuiOutlinedInput-root:onfocus': {
+                  '& > fieldset': { color: 'white' },
+                },
               }}
+              color="secondary"
+              label="Email address"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <TextField
+              sx={{
+                input: { color: 'white' },
+                '& .MuiInputLabel-root': {
+                  color: theme.palette.secondary.main,
+                }, //  styles the label
+                '& .MuiOutlinedInput-root': {
+                  '& > fieldset': {
+                    borderColor: theme.palette.secondary.main,
+                  },
+                },
+                '& .MuiOutlinedInput-root:hover': {
+                  '& > fieldset': {
+                    borderColor: theme.palette.secondary.main,
+                  },
+                },
+                '& .MuiOutlinedInput-root:onfocus': {
+                  '& > fieldset': { color: theme.palette.secondary.main },
+                },
+              }}
+              color="secondary"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <div id="buttonContainer">
+              <Button
+                id="loginBtn"
+                type="submit"
+                variant="contained"
+                style={{
+                  backgroundColor: `${theme.palette.secondary.main}`,
+                  color: `${theme.palette.primary.main}`,
+                }}
+              >
+                Log In
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div className="linkContainer">
+          <div className="signupRedirect">
+            <p>Don&apos;t have an account yet?</p>
+            <Link
+              to="/signup"
+              style={{ color: `${theme.palette.secondary.main}` }}
             >
-              <TextField
-                sx={{
-                  input: { color: 'white' },
-                  '& .MuiInputLabel-root': {
-                    color: theme.palette.secondary.main,
-                  }, //  styles the label
-                  '& .MuiOutlinedInput-root': {
-                    '& > fieldset': {
-                      borderColor: theme.palette.secondary.main,
-                    },
-                  },
-                  '& .MuiOutlinedInput-root:hover': {
-                    '& > fieldset': {
-                      borderColor: theme.palette.secondary.main,
-                    },
-                  },
-                  '& .MuiOutlinedInput-root:onfocus': {
-                    '& > fieldset': { color: 'white' },
-                  },
-                }}
-                color="secondary"
-                label="Email address"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <TextField
-                sx={{
-                  input: { color: 'white' },
-                  '& .MuiInputLabel-root': {
-                    color: theme.palette.secondary.main,
-                  }, //  styles the label
-                  '& .MuiOutlinedInput-root': {
-                    '& > fieldset': {
-                      borderColor: theme.palette.secondary.main,
-                    },
-                  },
-                  '& .MuiOutlinedInput-root:hover': {
-                    '& > fieldset': {
-                      borderColor: theme.palette.secondary.main,
-                    },
-                  },
-                  '& .MuiOutlinedInput-root:onfocus': {
-                    '& > fieldset': { color: theme.palette.secondary.main },
-                  },
-                }}
-                color="secondary"
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <div id="buttonContainer">
-                <Button
-                  id="loginBtn"
-                  type="submit"
-                  variant="contained"
-                  style={{
-                    backgroundColor: `${theme.palette.secondary.main}`,
-                    color: `${theme.palette.primary.main}`,
-                  }}
-                >
-                  Log In
-                </Button>
-              </div>
-            </form>
+              Sign Up
+            </Link>
           </div>
-          <div className="linkContainer">
-            <div className="signupRedirect">
-              <p>Don&apos;t have an account yet?</p>
-              <Link
-                to="/signup"
-                style={{ color: `${theme.palette.secondary.main}` }}
-              >
-                Sign Up
-              </Link>
-            </div>
-            <div className="recoveryRedirect">
-              <p>Forgot your password?</p>
-              <Link
-                to="/recovery"
-                style={{ color: `${theme.palette.secondary.main}` }}
-              >
-                Help Me DEVs!
-              </Link>
-            </div>
+          <div className="recoveryRedirect">
+            <p>Forgot your password?</p>
+            <Link
+              to="/recovery"
+              style={{ color: `${theme.palette.secondary.main}` }}
+            >
+              Help Me DEVs!
+            </Link>
           </div>
-        </Box>
-      </div>
-    </>
+        </div>
+      </Box>
+    </div>
   );
 }
 
